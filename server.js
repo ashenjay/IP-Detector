b11f3af984c667ce81bf877a760aebf19666bdde
Ashen Vithrana <90669153+ashenjay@users.noreply.github.com>
2025-08-11 18:29:39 +0530

    Increase database connection timeout

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ const pool = new Pool({
   ssl: config.nodeEnv === 'production' ? { require: true, rejectUnauthorized: false } : false,
   max: 20,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 60000,
+  connectionTimeoutMillis: 120000,
 });
 
 // Test database connection
